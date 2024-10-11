75dfd13559804a7bcc3156ae435675a3fb0d4c2d
Katia <katia.punter@storyshareplatform.com>
2024-10-11 13:36:52 +0100

    Passes test 2

--- helperPrime.js
+++ helperPrime.js
@@ -1,3 +1,10 @@
-const isPrime = (number) => false; // First step: Return false for everything!
+const isPrime = (number) => {
+    if (number === 0 || number === 1) {
+        return false;
+    } else {
+        return true; 
+    }
+    
+}
 
 module.exports = isPrime;
